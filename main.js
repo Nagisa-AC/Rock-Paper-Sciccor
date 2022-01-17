6d4ac46bfedb3fc005a257652d9d333c47560a57
Nagisa-AC <engineeredlie@gmail.com>
2022-01-17 08:16:04 -0500

    Implemented computerPlay() function to allow computer to have its turn

---
+++ main.js
@@ -0,0 +1,5 @@
+// Allow compute to play
+function computerPlay() {
+    const gameChoices = ["Rock", "Paper", "Scissors"];
+    return gameChoices[Math.floor(Math.random() * gameChoices.length)]
+}
