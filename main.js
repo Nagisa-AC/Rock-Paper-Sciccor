c219364467b7c9393075737af63565f7000ac111
Nagisa-AC <engineeredlie@gmail.com>
2022-01-17 08:33:37 -0500

    Added function skeleon for playRound() along with exception if user enters invalid input

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ function computerPlay() {
     const gameChoices = ["Rock", "Paper", "Scissors"];
     return gameChoices[Math.floor(Math.random() * gameChoices.length)]
 }
+
+/** Allow user to play round */
+function playRound(playerSelection, computerSelection) {
+    let modifiedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    const gameChoices = ["Rock", "Paper", "Scissors"];
+
+    if (!(gameChoices.includes(modifiedPlayerSelection))) {
+        throw `Value not supported. Value must be one of  
+            Rock, Paper, Scissors`
+    }
+
+    
+}
+
+
+// If user input != one of three choices, raise an error
+    // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+// Run conditional tests on RPS. For example --> (See below)
+    // Rock > Scissor
+    // Paper > Rock
+    // Scissor > Paper
